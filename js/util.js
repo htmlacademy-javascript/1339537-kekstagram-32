a16d6c6adb9bf8647078514816d56902dfcc09f1
Galina Popova <g89117301783@gmail.com>
2024-08-03 17:35:49 +0300

    Добавит  отказ от использования временных данных для разработки и создать модуль для работы с сервером

--- js/util.js
+++ js/util.js
@@ -1,22 +1,41 @@
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a,b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+const ALERT_SHOW_TIME = 5000;
+
+const dataErrorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
+
+const showAlert = () => {
+  const dataErrorElement = dataErrorTemplate.cloneNode(true);
+  document.body.append(dataErrorElement);
+
+  setTimeout(() => {
+    dataErrorElement.remove();
+  }, ALERT_SHOW_TIME);
 };
 
-const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
+export {showAlert};
 
-const createIdGenerator = () => {
-  let lastGeneratedId = 0;
-  return () => {
-    lastGeneratedId += 1;
-    return lastGeneratedId;
-  };
-};
+// const
 
-export {
-  getRandomInteger,
-  getRandomArrayElement,
-  createIdGenerator,
-};
+// const
+
+// const getRandomInteger = (a, b) => {
+//   const lower = Math.ceil(Math.min(a,b));
+//   const upper = Math.floor(Math.max(a, b));
+//   const result = Math.random() * (upper - lower + 1) + lower;
+//   return Math.floor(result);
+// };
+
+// const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
+
+// const createIdGenerator = () => {
+//   let lastGeneratedId = 0;
+//   return () => {
+//     lastGeneratedId += 1;
+//     return lastGeneratedId;
+//   };
+// };
+
+// export {
+//   getRandomInteger,
+//   getRandomArrayElement,
+//   createIdGenerator,
+// };
