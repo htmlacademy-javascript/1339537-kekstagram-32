68d905889c7ae8f4eac21ecbaee098a0c63d08c6
Galina Popova <g89117301783@gmail.com>
2024-08-08 19:34:14 +0300

    Добавит незачтённые критерии

--- js/message.js
+++ js/message.js
@@ -2,7 +2,7 @@ const successMessage = document.querySelector('#success').content.querySelector(
 const errorMessage = document.querySelector('#error').content.querySelector('.error');
 const body = document.querySelector('body');
 
-function hideMessage() {
+function onHideMessageButtonClick() {
   const messageElement = document.querySelector('.success') || document.querySelector('.error');
   messageElement.remove();
   document.removeEventListener('keydown', onDocumentKeydown);
@@ -13,13 +13,13 @@ function onBodyClick(evt) {
   if (evt.target.closest('.success__inner') || evt.target.closest('.error__inner')) {
     return;
   }
-  hideMessage();
+  onHideMessageButtonClick();
 }
 
 function onDocumentKeydown(evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
-    hideMessage();
+    onHideMessageButtonClick();
   }
 }
 
@@ -27,7 +27,7 @@ const showMessage = (messageElement, closeButtonClass) => {
   body.append(messageElement);
   document.addEventListener('keydown', onDocumentKeydown);
   body.addEventListener('click', onBodyClick);
-  messageElement.querySelector(closeButtonClass).addEventListener('click', hideMessage);
+  messageElement.querySelector(closeButtonClass).addEventListener('click', onHideMessageButtonClick);
 };
 
 const showSuccessMessage = () => {
